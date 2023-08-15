fa360d2de34c88ddc4cafec27f578eba135aefc5
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-08-15 10:59:22 -0500

    Added Comments

--- src/main/java/useless/prismaticlibe/IColored.java
+++ src/main/java/useless/prismaticlibe/IColored.java
@@ -1,9 +1,10 @@
 package useless.prismaticlibe;
 
+// Items that implement IColored will use the colored item renderer
 public interface IColored {
-    public int baseColor();
-    public int overlayColor();
+    public int baseColor(); // 24bit color represent by an integer
+    public int overlayColor(); // 24bit color represent by an integer
 
-    public int[] baseTexture();
-    public int[] overlayTexture();
+    public int[] baseTexture(); // Texture coordinates
+    public int[] overlayTexture(); // Texture coordinates
 }
--- src/main/java/useless/prismaticlibe/PrismaticLibe.java
+++ src/main/java/useless/prismaticlibe/PrismaticLibe.java
@@ -7,13 +7,6 @@ import org.slf4j.LoggerFactory;
 
 
 public class PrismaticLibe implements ModInitializer {
-    /*static {
-        //this is here to possibly fix some class loading issues, do not delete
-        try {
-            Class.forName("net.minecraft.core.block.Block");
-            Class.forName("net.minecraft.core.item.Item");
-        } catch (ClassNotFoundException ignored) {}
-    }*/
     public static final String MOD_ID = "prismaticlibe";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
