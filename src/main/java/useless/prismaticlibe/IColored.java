9a4bc2233cf732b9f76f1016dd0726530b7ec205
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-12-02 13:00:53 -0600

    Removed old AtlasLib support mode

--- src/main/java/useless/prismaticlibe/IColored.java
+++ src/main/java/useless/prismaticlibe/IColored.java
@@ -2,9 +2,9 @@ package useless.prismaticlibe;
 
 // Items that implement IColored will use the colored item renderer
 public interface IColored {
-    public int baseColor(); // 24bit color represent by an integer
-    public int overlayColor(); // 24bit color represent by an integer
+    int baseColor(); // 24bit color represent by an integer
+    int overlayColor(); // 24bit color represent by an integer
 
-    public int[] baseTexture(); // Texture coordinates
-    public int[] overlayTexture(); // Texture coordinates
+    int[] baseTexture(); // Texture coordinates
+    int[] overlayTexture(); // Texture coordinates
 }
