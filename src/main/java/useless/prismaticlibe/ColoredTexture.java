dbd6a604c708192831b37fd026f879ca9ebd099d
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-12-02 18:46:03 -0600

    Easy mod textures

--- src/main/java/useless/prismaticlibe/ColoredTexture.java
+++ src/main/java/useless/prismaticlibe/ColoredTexture.java
@@ -1,6 +1,7 @@
 package useless.prismaticlibe;
 
 import net.minecraft.core.block.Block;
+import turniplabs.halplibe.helper.TextureHelper;
 
 import java.awt.*;
 
@@ -10,6 +11,9 @@ public class ColoredTexture {
     public ColoredTexture(int[] textureCoordinates, Color color){
         this.textureCoordinates = textureCoordinates;
         this.color = color;
+    }
+    public ColoredTexture(String modID, String itemTextureSource, Color color){
+        this(TextureHelper.getOrCreateItemTexture(modID, itemTextureSource), color);
     }
     public int getTextureIndex(){
         return Block.texCoordToIndex(textureCoordinates[0], textureCoordinates[1]);
