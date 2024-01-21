d418d2bdbef064641704ef13693d11ee2b21c007
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-01-21 13:34:56 -0600

    Update PrismaticLibe.java

--- src/main/java/useless/prismaticlibe/PrismaticLibe.java
+++ src/main/java/useless/prismaticlibe/PrismaticLibe.java
@@ -1,6 +1,7 @@
 package useless.prismaticlibe;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import useless.prismaticlibe.debug.DebugMain;
@@ -11,10 +12,16 @@ public class PrismaticLibe implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static int playerArmorRenderOffset = 0;
 
-
+    public static boolean isDev;
+    static {
+        String version = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getVersion().getFriendlyString();
+        isDev = version.equals("${version}") || version.contains("dev");
+    }
     @Override
     public void onInitialize() {
-//        DebugMain.init();
+        if (isDev){
+            DebugMain.init();
+        }
         LOGGER.info("PrismaticLibe initialized.");
     }
 }
